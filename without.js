b8d0f570b13232e8803ecf4321531deb7f16c073
Your Name <your@email.com>
2021-11-25 13:38:38 -0500

    Added eqArrays test to without.js

--- without.js
+++ without.js
@@ -1,14 +1,14 @@
-// const eqArrays = function(array1, array2) {
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] === array2[i]) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-// };
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
-const assertArraysEqual = function (array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   let pass = true; // declare variable to check if arrays remain equal
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -27,7 +27,7 @@ const assertArraysEqual = function (array1, array2) {
 //if sourceArray values do not equal items to remove values, push them into a new Array.
 
 //third ATTEMPT // using nested for loops, .slice(), .splice(i,1)
-const without = function (sourceArray, itemsToRemove) {
+const without = function(sourceArray, itemsToRemove) {
   let newArray = sourceArray.slice();
   for (let i = 0; i < newArray.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
@@ -45,6 +45,10 @@ without(words, ["lighthouse"]); // no need to capture return value for this test
 assertArraysEqual(words, ["hello", "world", "lighthouse"]); //✅✅✅ Assertion Passed: hello,world,lighthouse === hello,world,lighthouse
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // ✅✅✅ Assertion Passed: 2,3 === 2,3
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // ✅✅✅ Assertion Passed: 1,2 === 1,2
+
+console.log(eqArrays(words, ["hello", "world", "lighthouse"]));
+console.log(eqArrays((without([1, 2, 3], [1])), [2, 3]));
+console.log(eqArrays(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]));
 
 // console.log(without([1, 2, 3], [1])); // => [2, 3]
 // console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
