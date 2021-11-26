297285e52c3b73ffc58230ddfc915920132d36f6
Your Name <your@email.com>
2021-11-26 07:58:04 -0500

    add takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,61 @@
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      break;
+    }
+    results.push(array[i]);
+  }
+  return results;
+};
+
+//Expected Input
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const assertArraysEqual = function(array1, array2) {
+  let pass = true; // declare variable to check if arrays remain equal
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      pass = false; // if an element is not the same change pass to false
+      break;
+    }
+  }
+  return (pass === true ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`));
+}; //if pass is still true, console log appropriate. Otherwise console log fail message.
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+console.log(eqArrays(results1, [ 1, 2, 5, 7, 2 ]));
+console.log(eqArrays(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]));
+
+//Expected Output
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+/* The function will return a "slice of the array with elements taken from the beginning."
+It should keep going until the callback/predicate returns a truthy value.
+
+To keep things simple, the callback should only be provided one value: The item in the array.
+
+Implement takeUntil which will keep collecting items from a provided array until
+the callback provided returns a truthy value. */
