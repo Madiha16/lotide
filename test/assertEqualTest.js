0ecdfa915cca811a741e43f04cad143e129719c9
Your Name <your@email.com>
2021-11-30 15:54:25 -0500

    added assertEqualTest

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("STRING", "string");
+assertEqual(3, 3);
+assertEqual(1, 5);
