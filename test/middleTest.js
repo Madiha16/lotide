92136a28ef5b41633b8c0c06d363f33fe3a8634a
Your Name <your@email.com>
2021-11-30 18:35:57 -0500

    added middleTest

---
+++ test/middleTest.js
@@ -0,0 +1,17 @@
+const middle  = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST CODE
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+let result1 = middle([1, 2, 3]);
+
+console.log(assertArraysEqual(result1, [2]));
+
+console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
