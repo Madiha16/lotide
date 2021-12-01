b420d3c4d45ad26124950eae8fdbf0d764cc859e
Your Name <your@email.com>
2021-11-30 22:04:54 -0500

    added tests to tailTest and middleTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -8,13 +8,17 @@ describe("#tail", () => {
   it(`returns ["Lighthouse", "Labs] for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
     assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
-  it(`returns [] for ["Labs"]`, () => {
+  it(`returns [] for one element ["Labs"]`, () => {
     assert.deepEqual(tail(["Labs"]), []);
   });
-  it("returns [] for []", () => {
+  it("returns [] for an empty array []", () => {
     assert.deepEqual(tail([]), []);
   });
-  it(`returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]`, () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  it(`returns [2, 3], the tail of an array (not the first element) for [1 ,2, 3]`, () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it(`returns 0 for an empty array`, () => {
+    const result1 = tail([]);
+    assert.equal(result1.length, 0);
   });
 });
